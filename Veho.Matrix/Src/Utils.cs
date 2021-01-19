c9d176ab63db0bd35e94a6fa1e04733de0cf6195
Hoyeung <hoyeungw@outlook.com>
2021-01-19 21:11:40 +0800

    add flatten

--- Veho.Matrix/Src/Utils.cs
+++ Veho.Matrix/Src/Utils.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Veho.Matrix {
   public static class Utils {
     public static T[,] Transpose<T>(this T[,] matrix) {
@@ -8,6 +10,26 @@ namespace Veho.Matrix {
         for (var j = 0; j < w; j++)
           target[j, i] = matrix[i, j];
       return target;
+    }
+
+    public static T[] Flatten<T>(this T[,] matrix) {
+      var (h, w) = matrix.Size();
+      var vec = new T[matrix.Length];
+      var label = 0;
+      for (var i = 0; i < h; i++)
+        for (var j = 0; j < w; j++)
+          vec[label++] = matrix[i, j];
+      return vec;
+    }
+
+    public static P[] Flatten<T, P>(this T[,] matrix, Func<T, P> fn) {
+      var (h, w) = matrix.Size();
+      var vec = new P[matrix.Length];
+      var label = 0;
+      for (var i = 0; i < h; i++)
+        for (var j = 0; j < w; j++)
+          vec[label++] = fn(matrix[i, j]);
+      return vec;
     }
   }
 }
