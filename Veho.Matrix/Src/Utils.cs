f01812d283f885a8451bbfafa914a2554898f118
hoyeungw <hoyeungw@outlook.com>
2021-01-22 22:49:10 +0800

    add functions to Matrix.Utils

--- Veho.Matrix/Src/Utils.cs
+++ Veho.Matrix/Src/Utils.cs
@@ -16,14 +16,13 @@ namespace Veho.Matrix {
     }
 
     public static T[] Flatten<T>(this T[,] matrix) {
-      // matrix.OfType<T>().ToArray();
       var (h, w) = matrix.Size();
       var vec = new T[matrix.Length];
       var label = 0;
       for (var i = 0; i < h; i++)
         for (var j = 0; j < w; j++)
           vec[label++] = matrix[i, j];
-      return vec;
+      return vec; // matrix.OfType<T>().ToArray();
     }
 
     public static P[] Flatten<T, P>(this T[,] matrix, Func<T, P> fn) {
@@ -35,7 +34,7 @@ namespace Veho.Matrix {
           vec[label++] = fn(matrix[i, j]);
       return vec;
     }
-    
+
     public static void Copy<T>(this T[,] matrix, T[,] target, int xStart = 0, int yStart = 0) {
       var wd = matrix.Width();
       for (var i = 0; i < matrix.Height(); i++) {
