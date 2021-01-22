7b068085e1354c55f6be2c3c9d43f5563c2b52e6
Hoyeung <hoyeungw@outlook.com>
2021-01-22 18:45:09 +0800

    minor fix

--- Veho.Matrix/Src/Columns.cs
+++ Veho.Matrix/Src/Columns.cs
@@ -10,16 +10,14 @@ namespace Veho.Matrix {
     //       fn(matrix[i,j]);
     //   
     // }
-    
-    public static T[] Column<T>(this T[,] matrix, int columnNumber)
-    {
+
+    public static T[] Column<T>(this T[,] matrix, int columnNumber) {
       return Enumerable.Range(0, matrix.GetLength(0))
         .Select(x => matrix[x, columnNumber])
         .ToArray();
     }
 
-    public static T[] Row<T>(this T[,] matrix, int rowNumber)
-    {
+    public static T[] Row<T>(this T[,] matrix, int rowNumber) {
       return Enumerable.Range(0, matrix.GetLength(1))
         .Select(x => matrix[rowNumber, x])
         .ToArray();
