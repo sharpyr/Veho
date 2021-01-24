f68511b51cd2556f8b16998b7921372a16faa224
hoyeungw <hoyeungw@outlook.com>
2021-01-24 16:57:28 +0800

    add Veho.Enumerable

---
+++ Veho.Enumerable/Src/Mappers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Veho.Enumerable {
+  public static class Mapper {
+    public static TO[] Map<T, TO>(this IEnumerable<T> en, Func<T, TO> func) {
+      return en.Select(func).ToArray();
+    }
+    public static void Iterate<T>(this IEnumerable<T> en, Action<T> func) {
+      foreach (var x in en) func(x);
+    }
+    public static void Iterate<T>(this IEnumerable<T> en, Action<int, T> func) {
+      var i = 0;
+      foreach (var x in en) func(i++, x);
+    }
+
+    public static int Hi<T>(this IEnumerable<T> en) => en.Count() - 1;
+  }
+}
---
+++ Veho.Vector/Src/ToMatrix.cs
@@ -0,0 +1,16 @@
+﻿namespace Veho.Vector {
+  public static class ToMatrix {
+    public static T[,] ToRow<T>(this T[] vector) {
+      var hi = vector.Length;
+      var row = new T[1, hi];
+      for (var j = 0; j < hi; j++) row[0, j] = vector[j];
+      return row;
+    }
+    public static T[,] ToColumn<T>(this T[] vector) {
+      var hi = vector.Length;
+      var column = new T[hi, 1];
+      for (var i = 0; i < hi; i++) column[i, 0] = vector[i];
+      return column;
+    }
+  }
+}
