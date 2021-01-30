a139a8d323b01861c8dd872f83bf66cf684b37a1
hoyeungw <hoyeungw@outlook.com>
2021-01-30 10:30:09 +0800

    add Veho.Tuple

---
+++ Veho.Tuple/Src/Helpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Veho.Tuple {
+  public static class Helpers {
+    public static (TO x, TO y) Map<T, TO>(this (T x, T y) t, Func<T, TO> f) => (f(t.x), f(t.y));
+    public static (TO x, TO y, TO z) Map<T, TO>(this (T x, T y, T z) t, Func<T, TO> f) => (f(t.x), f(t.y), f(t.z));
+    public static void Iterate<T, TO>(ref (T x, T y, T z) t, Action<T> f) {
+      f(t.x);
+      f(t.y);
+      f(t.z);
+    }
+  }
+}
