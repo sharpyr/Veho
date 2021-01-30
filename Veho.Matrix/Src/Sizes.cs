db70d66c0c4c1644e31a48768bac4dce949622f2
hoyeungw <hoyeungw@outlook.com>
2021-01-30 10:57:34 +0800

    minor fix

--- Veho.Matrix/Src/Sizes.cs
+++ Veho.Matrix/Src/Sizes.cs
@@ -4,7 +4,7 @@ namespace Veho.Matrix {
   public static class Sizes {
     public static bool Any(this (int height, int width) size) => size.height != 0 && size.width != 0;
 
-    public static void Iter<T>(this (int height, int width) size, Action<int, int> fn) {
+    public static void Iter(this (int height, int width) size, Action<int, int> fn) {
       var (h, w) = size;
       for (var i = 0; i < h; i++)
         for (var j = 0; j < w; j++)
