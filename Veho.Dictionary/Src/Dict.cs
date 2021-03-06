855bc64a191a5cdf38c5dbcdeb8f3983a0d2c669
hoyeungw <hoyeungw@outlook.com>
2021-03-07 00:47:08 +0800

    add Dict to Veho.Dictionary

---
+++ Veho.Dictionary/Src/Dict.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Veho.Dictionary {
+  public static class Dict {
+    public static Dictionary<TK, TV> From<TK, TV>(params (TK key, TV value)[] entries) => entries
+      .ToDict();
+    public static Dictionary<TK, TV> ToDict<TK, TV>(this (TK key, TV value)[] entries) {
+      var dict = new Dictionary<TK, TV>();
+      for (int i = 0, hi = entries.Length; i < hi; i++) {
+        var (k, v) = entries[i];
+        dict.Add(k, v);
+      }
+      return dict;
+    }
+  }
+}
