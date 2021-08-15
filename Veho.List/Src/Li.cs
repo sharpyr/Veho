954d6a5206c01b3433c9f86bdd85ecb00d1e2a80
hoyeungw <hoyeungw@outlook.com>
2021-08-15 22:07:12 +0800

    add Li

---
+++ Veho.List/Src/Li.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using G = System.Collections.Generic;
+
+namespace Veho {
+  public static class Li {
+    public static G::List<T> Iso<T>(int len, T element) {
+      var list = new G::List<T>(len);
+      for (var i = 0; i < len; i++) list.Add(element);
+      return list;
+    }
+    public static G::List<T> Init<T>(int len, Func<int, T> func) {
+      var list = new G::List<T>(len);
+      for (var i = 0; i < len; i++) list.Add(func(i));
+      return list;
+    }
+  }
+}
