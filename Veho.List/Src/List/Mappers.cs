3def0c0f4ae1f20a5238d8bc8a09797c4e00b1e5
hoyeungw <hoyeungw@outlook.com>
2021-08-15 22:09:05 +0800

    add Mappers for Veho.Mutable.Matrix

--- Veho.List/Src/List/Mappers.cs
+++ Veho.List/Src/List/Mappers.cs
@@ -7,20 +7,27 @@ namespace Veho.List {
     //
     // IList(of T)
     //
-    public static TOut[] Map<T, TOut>(this IList<T> list, Func<T, TOut> func) {
-      var vec = new TOut[list.Count];
-      var arr = list.ToArray();
+    public static TOut[] MapToArray<T, TOut>(this IList<T> list, Func<T, TOut> func) {
+      var tar = new TOut[list.Count];
       for (var i = 0; i < list.Count; i++)
-        vec[i] = func(arr[i]);
-      return vec;
+        tar[i] = func(list.ElementAt(i));
+      return tar;
+    }
+    public static List<TOut> Map<T, TOut>(this List<T> list, Func<T, TOut> func) {
+      var tar = new List<TOut>(list.Count);
+      for (int i = 0, hi = list.Count; i < hi; i++) tar[i] = func(list[i]);
+      return tar;
+    }
+    public static List<TOut> Map<T, TOut>(this List<T> list, Func<int, T, TOut> fn) {
+      var tar = new List<TOut>(list.Count);
+      for (int i = 0, hi = list.Count; i < hi; i++) tar[i] = fn(i, list[i]);
+      return tar;
     }
     public static void Iterate<T>(this IList<T> list, Action<T> func) {
-      foreach (var cel in list)
-        func(cel);
+      foreach (var cel in list) func(cel);
     }
-    public static void Iterate<T>(this IList<T> list, Action<T, int> func) {
-      var arr = list.ToArray();
-      for (var i = 0; i < list.Count; i++) func(arr[i], i);
+    public static void Iterate<T>(this IList<T> list, Action<int, T> func) {
+      for (var i = 0; i < list.Count; i++) func(i, list.ElementAt(i));
     }
     public static void IterateBack<T>(this IList<T> list, Action<T> func) {
       for (var i = list.Hi(); i >= 0; i--) func(list[i]);
