079b5e35e2d82fed911bf7c271159dbd5c78d98f
hoyeungw <hoyeungw@outlook.com>
2021-08-15 22:08:44 +0800

    add Indexes for Veho.Mutable.Matrix

---
+++ Veho.Mutable.Matrix/Matrix/Indexers.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Veho.Mutable.Matrix {
+  public static class Indexers {
+    public static T First<T>(this List<List<T>> rows) => rows[0][0];
+    public static T Cell<T>(this List<List<T>> rows, int x, int y) => rows[x][y];
+  }
+}
