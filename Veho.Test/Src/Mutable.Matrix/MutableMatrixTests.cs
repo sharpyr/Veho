87378f3a9a4dca8d057121adaef15b6de0c1df2c
hoyeungw <hoyeungw@outlook.com>
2021-08-16 08:43:42 +0800

    add test

---
+++ Veho.Test/Src/Mutable.Matrix/MutableMatrixTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using Veho.Mutable.Matrix;
+
+namespace Veho.Test.Mutable.Matrix {
+  [TestFixture]
+  public class MutableMatrixTests {
+    [Test]
+    public void SomeTest() {
+      var matrix = Li.From(
+        Li.From(1, 1, 2, 3, 5, 8, 13, 21),
+        Li.From(1, 1, 2, 3, 5, 8, 13, 21),
+        Li.From(1, 1, 2, 3, 5, 8, 13, 21)
+      );
+      matrix = matrix.Map(x => x * 2);
+      var (height, width) = matrix.Size();
+      for (var i = 0; i < height; i++) {
+        for (var j = 0; j < width; j++) {
+          var value = matrix[i][j];
+          Console.Write($"{value}, ");
+        }
+        Console.WriteLine();
+      }
+      Console.WriteLine("passed");
+    }
+  }
+}
