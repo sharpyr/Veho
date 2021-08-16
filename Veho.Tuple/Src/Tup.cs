a92a16957bb63d7824759cdf7cc7f442e2f890ea
hoyeungw <hoyeungw@outlook.com>
2021-08-16 08:43:37 +0800

    add ToList for Veho.Tup

--- Veho.Tuple/Src/Tup.cs
+++ Veho.Tuple/Src/Tup.cs
@@ -1,4 +1,6 @@
-﻿namespace Veho {
+﻿using System.Collections.Generic;
+
+namespace Veho {
   public static class Tup {
     public static T[] ToVector<T>(this (T, T) entry) {
       var (x, y) = entry;
@@ -7,6 +9,23 @@
     public static T[] ToVector<T>(this (T, T, T) entry) {
       var (x, y, z) = entry;
       return new[] {x, y, z};
+    }
+    public static T[] ToVector<T>(this (T, T, T, T) entry) {
+      var (a, b, c, d) = entry;
+      return new[] {a, b, c, d};
+    }
+
+    public static List<T> ToList<T>(this (T, T) entry) {
+      var (x, y) = entry;
+      return new List<T> {x, y};
+    }
+    public static List<T> ToList<T>(this (T, T, T) entry) {
+      var (x, y, z) = entry;
+      return new List<T> {x, y, z};
+    }
+    public static List<T> ToList<T>(this (T, T, T, T) entry) {
+      var (a, b, c, d) = entry;
+      return new List<T> {a, b, c, d};
     }
   }
 }
