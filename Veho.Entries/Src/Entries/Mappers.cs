fa52fa97c7d3b408f66aae1644f7af3798ccda6a
hoyeungw <hoyeungw@outlook.com>
2021-08-16 11:50:08 +0800

    add Mappers for Entries

---
+++ Veho.Entries/Src/Entries/Mappers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Veho.Vector;
+
+namespace Veho.Entries {
+  public static class Mappers {
+    public static TK[] Keys<TK, TV>(this (TK key, TV value)[] entries) => entries.Map(kv => kv.key);
+    public static TV[] Values<TK, TV>(this (TK key, TV value)[] entries) => entries.Map(kv => kv.value);
+
+    public static IEnumerable<TK> Keys<TK, TV>(this IEnumerable<(TK key, TV value)> entries) => entries.Select(kv => kv.key);
+    public static IEnumerable<TV> Values<TK, TV>(this IEnumerable<(TK key, TV value)> entries) => entries.Select(kv => kv.value);
+  }
+}
