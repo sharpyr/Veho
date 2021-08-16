50edbb7b0fabc7d7d8a9643e6eeb2e73910054e4
hoyeungw <hoyeungw@outlook.com>
2021-08-16 08:41:56 +0800

    fix list api unmatch

--- Veho.Mutable.Matrix/Columns/Indexers.cs
+++ Veho.Mutable.Matrix/Columns/Indexers.cs
@@ -6,19 +6,19 @@ namespace Veho.Mutable.Columns {
     public static List<T> Column<T>(this List<List<T>> rows, int y) {
       var h = rows.Count;
       var col = new List<T>(rows.Count);
-      for (var i = 0; i < h; i++) col[i] = rows[i][y];
+      for (var i = 0; i < h; i++) col.Add(rows[i][y]);
       return col;
     }
     public static List<TO> Column<T, TO>(this List<List<T>> rows, int y, Func<T, TO> fn) {
       var h = rows.Count;
       var col = new List<TO>(rows.Count);
-      for (var i = 0; i < h; i++) col[i] = fn(rows[i][y]);
+      for (var i = 0; i < h; i++) col.Add(fn(rows[i][y]));
       return col;
     }
     public static List<TO> Column<T, TO>(this List<List<T>> rows, int y, Func<int, T, TO> fn) {
       var h = rows.Count;
       var col = new List<TO>(rows.Count);
-      for (var i = 0; i < h; i++) col[i] = fn(i, rows[i][y]);
+      for (var i = 0; i < h; i++) col.Add(fn(i, rows[i][y]));
       return col;
     }
   }
