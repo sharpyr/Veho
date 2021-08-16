0199ab82464a4106f6fd90a7d79aaf4cbf8dbfe2
hoyeungw <hoyeungw@outlook.com>
2021-08-16 08:43:03 +0800

    add Veho.Mutable.Columns updater

--- Veho.Mutable.Matrix/Columns/Updaters.cs
+++ Veho.Mutable.Matrix/Columns/Updaters.cs
@@ -1,3 +1,18 @@
-﻿namespace Veho.Mutable.Columns {
-  public static class Updaters { }
+﻿using System.Collections.Generic;
+
+namespace Veho.Mutable.Columns {
+  public static class Updaters {
+    public static List<List<T>> UnshiftColumn<T>(this List<List<T>> matrix, List<T> vec, int y) {
+      for (int i = 0, hi = vec.Count; i < hi; i++) matrix[i].Insert(0, vec[i]);
+      return matrix;
+    }
+    public static List<List<T>> PushColumn<T>(this List<List<T>> matrix, List<T> vec, int y) {
+      for (int i = 0, hi = vec.Count; i < hi; i++) matrix[i].Add(vec[i]);
+      return matrix;
+    }
+    public static List<List<T>> WriteColumn<T>(this List<List<T>> matrix, List<T> vec, int y) {
+      for (int i = 0, hi = vec.Count; i < hi; i++) matrix[i][y] = vec[i];
+      return matrix;
+    }
+  }
 }
