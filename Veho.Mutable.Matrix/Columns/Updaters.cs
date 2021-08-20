7b813c65a737734d276361923b2c9500465d7809
hoyeungw <hoyeungw@outlook.com>
2021-08-20 11:48:28 +0800

    minor fix

--- Veho.Mutable.Matrix/Columns/Updaters.cs
+++ Veho.Mutable.Matrix/Columns/Updaters.cs
@@ -2,11 +2,11 @@
 
 namespace Veho.Mutable.Columns {
   public static class Updaters {
-    public static List<List<T>> UnshiftColumn<T>(this List<List<T>> matrix, List<T> vec, int y) {
+    public static List<List<T>> UnshiftColumn<T>(this List<List<T>> matrix, List<T> vec) {
       for (int i = 0, hi = vec.Count; i < hi; i++) matrix[i].Insert(0, vec[i]);
       return matrix;
     }
-    public static List<List<T>> PushColumn<T>(this List<List<T>> matrix, List<T> vec, int y) {
+    public static List<List<T>> PushColumn<T>(this List<List<T>> matrix, List<T> vec) {
       for (int i = 0, hi = vec.Count; i < hi; i++) matrix[i].Add(vec[i]);
       return matrix;
     }
