e1eadb6fe3c724e687e09d211395dd22d4870985
hoyeungw <hoyeungw@outlook.com>
2021-08-30 08:35:09 +0800

    add FlatMap

--- Veho.Mutable.Matrix/Matrix/Reducers.cs
+++ Veho.Mutable.Matrix/Matrix/Reducers.cs
@@ -1,8 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using Veho.Sequence;
+using SEQ = Veho.Sequence;
 
 namespace Veho.Mutable.Matrix {
   public static partial class Reducers {
+    public static List<T> Flat<T>(this List<List<T>> rows) {
+      var (h, w) = rows.Size();
+      return SEQ::Reducers.Fold(rows, (accum, row) => {
+        accum.AddRange(row);
+        return accum;
+      }, new List<T>(h * w));
+    }
+    public static List<TO> FlatMap<T, TO>(this List<List<T>> rows, Func<T, TO> func) {
+      var (h, w) = rows.Size();
+      return SEQ::Reducers.Fold(rows, (accum, row) => {
+        accum.AddRange(row.Map(func));
+        return accum;
+      }, new List<TO>(h * w));
+    }
+    public static List<TO> FlatMap<T, TO>(this List<List<T>> rows, Func<int, T, TO> func) {
+      var (h, w) = rows.Size();
+      var i = 0;
+      return SEQ::Reducers.Fold(rows, (accum, row) => {
+        row.Iterate(x => accum.Add(func(i++, x)));
+        return accum;
+      }, new List<TO>(h * w));
+    }
+
     public static TO Fold<T, TO>(this List<List<T>> rows, Func<TO, T, TO> sequence, TO acc) {
       for (int i = 0, h = rows.Count; i < h; i++) {
         var row = rows[i];
