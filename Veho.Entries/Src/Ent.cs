43b5ac6c57faa9dcf0bf3d985ed4652b39548b67
hoyeungw <hoyeungw@outlook.com>
2021-08-31 10:28:57 +0800

    use IReadOnlyList<T> instead of List<T>

--- Veho.Entries/Src/Ent.cs
+++ Veho.Entries/Src/Ent.cs
@@ -1,24 +1,9 @@
-﻿namespace Veho {
-  public static class Ent {
-    // public static T[] ToVector<T>(this (T, T) entry) {
-    //   var (x, y) = entry;
-    //   return new[] {x, y};
-    // }
-    // public static T[] ToVector<T>(this (T, T, T) entry) {
-    //   var (x, y, z) = entry;
-    //   return new[] {x, y, z};
-    // }
-    // public static (TO, TO) Map<T, TO>(this (T, T) entry, Func<T, TO> f) {
-    //   var (x, y) = entry;
-    //   return (f(x), f(y));
-    // }
-    // public static (TO, TO, TO) Map<T, TO>(this (T, T, T) entry, Func<T, TO> f) {
-    //   var (x, y, z) = entry;
-    //   return (f(x), f(y), f(z));
-    // }
+﻿using System.Collections.Generic;
 
-    public static (TK[], TV[]) Unwind<TK, TV>(this (TK, TV)[] entries) {
-      var len = entries.Length;
+namespace Veho {
+  public static class Ent {
+    public static (TK[], TV[]) Unwind<TK, TV>(this IReadOnlyList<(TK, TV)> entries) {
+      var len = entries.Count;
       var keys = new TK[len];
       var values = new TV[len];
       for (var i = 0; i < len; i++) {
@@ -28,10 +13,52 @@
       }
       return (keys, values);
     }
-    public static (TK, TV)[] Wind<TK, TV>(TK[] keys, TV[] values) {
-      var hi = keys.Length;
+    public static (TK, TV)[] Wind<TK, TV>(IReadOnlyList<TK> keys, IReadOnlyList<TV> values) {
+      var hi = keys.Count;
       var entries = new (TK, TV)[hi];
       for (var lo = 0; lo < hi; lo++) entries[lo] = (keys[lo], values[lo]);
+      return entries;
+    }
+
+    public static (TA[] a, TB[] b, TC[] c) Unwind<TA, TB, TC>(this IReadOnlyList<(TA, TB, TC)> entries) {
+      var len = entries.Count;
+      var vecA = new TA[len];
+      var vecB = new TB[len];
+      var vecC = new TC[len];
+      for (var i = 0; i < len; i++) {
+        var (a, b, c) = entries[i];
+        vecA[i] = a;
+        vecB[i] = b;
+        vecC[i] = c;
+      }
+      return (vecA, vecB, vecC);
+    }
+    public static (TA a, TB b, TC c)[] Wind<TA, TB, TC>(IReadOnlyList<TA> a, IReadOnlyList<TB> b, IReadOnlyList<TC> c) {
+      var hi = a.Count;
+      var entries = new (TA, TB, TC)[hi];
+      for (var lo = 0; lo < hi; lo++) entries[lo] = (a[lo], b[lo], c[lo]);
+      return entries;
+    }
+
+    public static (TA[] a, TB[] b, TC[] c, TD[] d) Unwind<TA, TB, TC, TD>(this IReadOnlyList<(TA, TB, TC, TD)> entries) {
+      var len = entries.Count;
+      var vecA = new TA[len];
+      var vecB = new TB[len];
+      var vecC = new TC[len];
+      var vecD = new TD[len];
+      for (var i = 0; i < len; i++) {
+        var (a, b, c, d) = entries[i];
+        vecA[i] = a;
+        vecB[i] = b;
+        vecC[i] = c;
+        vecD[i] = d;
+      }
+      return (vecA, vecB, vecC, vecD);
+    }
+    public static (TA a, TB b, TC c, TD d)[] Wind<TA, TB, TC, TD>(IReadOnlyList<TA> a, IReadOnlyList<TB> b, IReadOnlyList<TC> c, IReadOnlyList<TD> d) {
+      var hi = a.Count;
+      var entries = new (TA, TB, TC, TD)[hi];
+      for (var lo = 0; lo < hi; lo++) entries[lo] = (a[lo], b[lo], c[lo], d[lo]);
       return entries;
     }
   }
--- Veho.Entries/Src/Entries/Convert.cs
+++ Veho.Entries/Src/Entries/Convert.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Veho.Entries {
   public static class Convert {
-    public static T[,] ToMatrix<T>(this List<(T key, T value)> entries) {
+    public static T[,] ToMatrix<T>(this IReadOnlyList<(T key, T value)> entries) {
       var hi = entries.Count;
       var target = new T[hi, 2];
       for (var i = 0; i < hi; i++) {
@@ -13,7 +13,7 @@ namespace Veho.Entries {
       }
       return target;
     }
-    public static TO[,] ToMatrix<T, TO>(this List<(T key, T value)> entries, Func<T, TO> func) {
+    public static TO[,] ToMatrix<T, TO>(this IReadOnlyList<(T key, T value)> entries, Func<T, TO> func) {
       var hi = entries.Count;
       var target = new TO[hi, 2];
       for (var i = 0; i < hi; i++) {
@@ -24,7 +24,7 @@ namespace Veho.Entries {
       return target;
     }
 
-    public static T[,] ToMatrix<T>(this List<(T a, T b, T c)> entries) {
+    public static T[,] ToMatrix<T>(this IReadOnlyList<(T a, T b, T c)> entries) {
       var hi = entries.Count;
       var target = new T[hi, 3];
       for (var i = 0; i < hi; i++) {
@@ -35,7 +35,7 @@ namespace Veho.Entries {
       }
       return target;
     }
-    public static TO[,] ToMatrix<T, TO>(this List<(T a, T b, T c)> entries, Func<T, TO> func) {
+    public static TO[,] ToMatrix<T, TO>(this IReadOnlyList<(T a, T b, T c)> entries, Func<T, TO> func) {
       var hi = entries.Count;
       var target = new TO[hi, 3];
       for (var i = 0; i < hi; i++) {
@@ -43,6 +43,31 @@ namespace Veho.Entries {
         target[i, 0] = func(a);
         target[i, 1] = func(b);
         target[i, 2] = func(c);
+      }
+      return target;
+    }
+
+    public static T[,] ToMatrix<T>(this IReadOnlyList<(T a, T b, T c, T d)> entries) {
+      var hi = entries.Count;
+      var target = new T[hi, 4];
+      for (var i = 0; i < hi; i++) {
+        var (a, b, c, d) = entries[i];
+        target[i, 0] = a;
+        target[i, 1] = b;
+        target[i, 2] = c;
+        target[i, 3] = d;
+      }
+      return target;
+    }
+    public static TO[,] ToMatrix<T, TO>(this IReadOnlyList<(T a, T b, T c, T d)> entries, Func<T, TO> func) {
+      var hi = entries.Count;
+      var target = new TO[hi, 4];
+      for (var i = 0; i < hi; i++) {
+        var (a, b, c, d) = entries[i];
+        target[i, 0] = func(a);
+        target[i, 1] = func(b);
+        target[i, 2] = func(c);
+        target[i, 3] = func(d);
       }
       return target;
     }
