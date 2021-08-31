f69e5d3340a7e342d68c3761850748658cae76a6
hoyeungw <hoyeungw@outlook.com>
2021-08-31 11:15:08 +0800

    add Swap for List<T>

---
+++ Veho.Sequence/Src/Sequence/Util.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Veho.Sequence {
+  public static class Util {
+    public static T Swap<T>(this IList<T> vector, int i, int j) {
+      var temp = vector[i];
+      vector[i] = vector[j];
+      return vector[j] = temp;
+    }
+  }
+}
