0ff4119a6f909ca71c45a9b982b6152aa297b0b4
hoyeungw <hoyeungw@outlook.com>
2021-08-31 10:29:25 +0800

    return IEnumerable<T> instead of T[] for Mappers

--- Veho.Dictionary/Src/Dict.cs
+++ Veho.Dictionary/Src/Dict.cs
@@ -1,22 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Veho {
   public static class Dict {
     public static Dictionary<TK, TV> From<TK, TV>(params (TK key, TV value)[] entries) => entries
       .ToDict();
-    public static Dictionary<TK, TV> ToDict<TK, TV>(this (TK key, TV value)[] entries) {
+    public static Dictionary<TK, TV> ToDict<TK, TV>(this IReadOnlyList<(TK key, TV value)> entries) {
       var dict = new Dictionary<TK, TV>();
-      for (int i = 0, hi = entries.Length; i < hi; i++) {
+      for (int i = 0, hi = entries.Count; i < hi; i++) {
         var (k, v) = entries[i];
         dict.Add(k, v);
       }
       return dict;
     }
 
-    public static (K[], V[]) Unwind<K, V>(this IDictionary<K, V> dict) {
+    public static (TK[], TV[]) Unwind<TK, TV>(this IDictionary<TK, TV> dict) {
       var len = dict.Count;
-      var keys = new K[len];
-      var values = new V[len];
+      var keys = new TK[len];
+      var values = new TV[len];
       var index = 0;
       foreach (var entry in dict) {
         keys[index] = entry.Key;
@@ -25,11 +26,18 @@ namespace Veho {
       return (keys, values);
     }
 
-    public static Dictionary<K, V> Wind<K, V>(K[] keys, V[] values) {
-      var hi = keys.Length;
-      var dict = new Dictionary<K, V>();
+    public static Dictionary<TK, TV> Wind<TK, TV>(IReadOnlyList<TK> keys, IReadOnlyList<TV> values) {
+      var hi = keys.Count;
+      var dict = new Dictionary<TK, TV>();
       for (var lo = 0; lo < hi; lo++) dict.Add(keys[lo], values[lo]);
       return dict;
+    }
+
+    public static IEnumerable<(TK key, TV value)> EnumEntries<TK, TV>(this IDictionary<TK, TV> dict) {
+      return dict.Select(kv => (kv.Key, kv.Value));
+    }
+    public static List<(TK key, TV value)> Entries<TK, TV>(this IDictionary<TK, TV> dict) {
+      return dict.Select(kv => (kv.Key, kv.Value)).ToList();
     }
   }
 }
--- Veho.Dictionary/Src/Dictionary/Mapper.cs
+++ Veho.Dictionary/Src/Dictionary/Mapper.cs
@@ -1,19 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Veho.Dictionary {
   public static class Mappers {
-    public static P[] MapKeys<K, _, P>(this IDictionary<K, _> dict, Func<K, P> fn) {
-      var vec = new P[dict.Count];
-      var i = 0;
-      foreach (var key in dict.Keys) vec[i++] = fn(key);
-      return vec;
+    public static IEnumerable<TP> Map<TK, TV, TP>(this IDictionary<TK, TV> dict, Func<TK, TV, TP> fn) {
+      return dict.Select(kv => fn(kv.Key, kv.Value));
     }
-    public static P[] MapValues<_, V, P>(this IDictionary<_, V> dict, Func<V, P> fn) {
-      var vec = new P[dict.Count];
-      var i = 0;
-      foreach (var value in dict.Values) vec[i++] = fn(value);
-      return vec;
+    public static IEnumerable<TP> MapKeys<TK, TV, TP>(this IDictionary<TK, TV> dict, Func<TK, TP> fn) {
+      return dict.Keys.Select(fn);
+    }
+    public static IEnumerable<TP> MapValues<TK, TV, TP>(this IDictionary<TK, TV> dict, Func<TV, TP> fn) {
+      return dict.Values.Select(fn);
     }
   }
 }
