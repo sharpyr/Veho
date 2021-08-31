ef001fc8ae96aa299d28c85a10d9c38fe44dd6f6
hoyeungw <hoyeungw@outlook.com>
2021-08-31 11:25:51 +0800

    add DistinctBy for IEnumerable<T>

---
+++ Veho.Enumerable/Src/Enumerable/Selectors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Veho.Enumerable {
+  public static class Selectors {
+    public static IEnumerable<T> DistinctBy<T, TK>(this IEnumerable<T> source, Func<T, TK> keySelector) {
+      var seenKeys = new HashSet<TK>();
+      foreach (var element in source) {
+        if (seenKeys.Add(keySelector(element))) {
+          yield return element;
+        }
+      }
+    }
+  }
+}
