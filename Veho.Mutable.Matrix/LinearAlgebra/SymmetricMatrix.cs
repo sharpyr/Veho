1f713e5970ef0b31a7e2ab8e1811fa0548d9ebfa
hoyeungw <hoyeungw@outlook.com>
2021-09-02 12:21:13 +0800

    add SelectBy

--- Veho.Mutable.Matrix/LinearAlgebra/SymmetricMatrix.cs
+++ Veho.Mutable.Matrix/LinearAlgebra/SymmetricMatrix.cs
@@ -4,6 +4,9 @@ using Veho.Sequence;
 
 namespace Veho.Mutable.LinearAlgebra {
   public static class SymmetricMatrix {
+    public static List<List<T>> SelectBy<T>(IReadOnlyList<IReadOnlyList<T>> rows, IReadOnlyList<int> indices) {
+      return rows.SelectBy(indices).Map(row => row.SelectBy(indices));
+    }
     public static HashSet<int> FindIntersectional<T>(this IReadOnlyList<IReadOnlyList<T>> rows,
                                                      int startIndex,
                                                      T signal,
