1425b4a60f0cde6053798ec7253d51131ea107b1
hoyeungw <hoyeungw@outlook.com>
2021-09-02 12:02:50 +0800

    add SymmetricMatrix

---
+++ Veho.Mutable.Matrix/LinearAlgebra/SymmetricMatrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Veho.Sequence;
+
+namespace Veho.Mutable.LinearAlgebra {
+  public static class SymmetricMatrix {
+    public static HashSet<int> FindIntersectional<T>(this IReadOnlyList<IReadOnlyList<T>> rows,
+                                                     int startIndex,
+                                                     T signal,
+                                                     HashSet<int> indices = null) where T : IEquatable<T> {
+      var row = rows[startIndex];
+      indices = indices ?? new HashSet<int>();
+      indices.Add(startIndex);
+      foreach (var i in row.FilterIndices(x => x.Equals(signal))) {
+        if (indices.Contains(i)) continue;
+        indices.UnionWith(FindIntersectional(rows, i, signal, indices));
+      }
+      return indices;
+    }
+  }
+}
