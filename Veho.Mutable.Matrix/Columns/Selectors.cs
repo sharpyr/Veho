be491e3d08c61cb3fc219cfaabce5b8a4f81eb85
hoyeungw <hoyeungw@outlook.com>
2021-09-02 11:34:51 +0800

    add Selectors

---
+++ Veho.Mutable.Matrix/Columns/Selectors.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Veho.Sequence;
+
+namespace Veho.Mutable.Columns {
+  public static class Selectors {
+    public static List<List<T>> SelectByColumnsBy<T>(this IReadOnlyList<IReadOnlyList<T>> rows, IReadOnlyList<int> columnIndices) {
+      return columnIndices.Map(rows.Column);
+    }
+    public static List<List<T>> SelectByColumnIndices<T>(this IReadOnlyList<IReadOnlyList<T>> rows, IReadOnlyList<int> columnIndices) {
+      return rows.Map(row => row.SelectBy(columnIndices));
+    }
+  }
+}
