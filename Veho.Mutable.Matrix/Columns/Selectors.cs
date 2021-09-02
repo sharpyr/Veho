a41e0dec653b01b8ae50f00c4701c5b3303d3b12
hoyeungw <hoyeungw@outlook.com>
2021-09-02 12:16:28 +0800

    renamed function

--- Veho.Mutable.Matrix/Columns/Selectors.cs
+++ Veho.Mutable.Matrix/Columns/Selectors.cs
@@ -3,7 +3,7 @@ using Veho.Sequence;
 
 namespace Veho.Mutable.Columns {
   public static class Selectors {
-    public static List<List<T>> SelectByColumnsBy<T>(this IReadOnlyList<IReadOnlyList<T>> rows, IReadOnlyList<int> columnIndices) {
+    public static List<List<T>> SelectColumnsBy<T>(this IReadOnlyList<IReadOnlyList<T>> rows, IReadOnlyList<int> columnIndices) {
       return columnIndices.Map(rows.Column);
     }
     public static List<List<T>> SelectByColumnIndices<T>(this IReadOnlyList<IReadOnlyList<T>> rows, IReadOnlyList<int> columnIndices) {
