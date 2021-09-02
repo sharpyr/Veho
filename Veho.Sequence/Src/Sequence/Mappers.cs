d64f79f070943d8512f64e8fbc9648a51c1d2c4b
hoyeungw <hoyeungw@outlook.com>
2021-09-02 11:34:23 +0800

    add Slice

--- Veho.Sequence/Src/Sequence/Mappers.cs
+++ Veho.Sequence/Src/Sequence/Mappers.cs
@@ -7,11 +7,8 @@ namespace Veho.Sequence {
     //
     // IList(of T)
     //
-    public static TOut[] MapToArray<T, TOut>(this IReadOnlyList<T> list, Func<T, TOut> func) {
-      var tar = new TOut[list.Count];
-      for (var i = 0; i < list.Count; i++)
-        tar[i] = func(list.ElementAt(i));
-      return tar;
+    public static List<T> Slice<T>(this IReadOnlyList<T> list) {
+      return list.Map(x => x);
     }
     public static List<TOut> Map<T, TOut>(this IReadOnlyList<T> list, Func<T, TOut> func) {
       var tar = new List<TOut>(list.Count);
@@ -21,6 +18,12 @@ namespace Veho.Sequence {
     public static List<TOut> Map<T, TOut>(this IReadOnlyList<T> list, Func<int, T, TOut> fn) {
       var tar = new List<TOut>(list.Count);
       for (int i = 0, hi = list.Count; i < hi; i++) tar.Add(fn(i, list[i]));
+      return tar;
+    }
+    public static TOut[] MapToArray<T, TOut>(this IReadOnlyList<T> list, Func<T, TOut> func) {
+      var tar = new TOut[list.Count];
+      for (var i = 0; i < list.Count; i++)
+        tar[i] = func(list.ElementAt(i));
       return tar;
     }
     public static void Iterate<T>(this IReadOnlyList<T> list, Action<T> func) {
