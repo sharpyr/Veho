8372277839b0c5262b59a321dd395b977f20bfce
hoyeungw <hoyeungw@outlook.com>
2021-09-02 17:07:21 +0800

    patch version

---
+++ Veho.Test/Src/Matrix/ConvertTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Spare;
+using Veho.Matrix;
+using Veho.Mutable.Matrix;
+
+namespace Veho.Test.Matrix {
+  [TestFixture]
+  public class ConvertTests {
+    [Test]
+    public void ToMutableMatrixTest() {
+      var matrix = Mat.Init(5, 5, (i, j) => i * j);
+      matrix.ToMutableMatrix().ToMatrix().Deco().Logger();
+      
+    }
+  }
+}
