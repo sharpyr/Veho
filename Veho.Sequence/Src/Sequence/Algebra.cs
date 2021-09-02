42d7086ce46445999241cd3cfd8763aab6bc27d7
hoyeungw <hoyeungw@outlook.com>
2021-09-02 15:16:17 +0800

    add Cartesian

---
+++ Veho.Sequence/Src/Sequence/Algebra.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Veho.Sequence {
+  public static class Algebra {
+    public static List<List<TO>> IndoCartesian<T, TO>(this IReadOnlyList<T> vec, Func<T, T, TO> func) {
+      var hi = vec.Count;
+      var mx = new List<List<TO>>(hi);
+      for (var i = 0; i < hi; i++) {
+        var x = vec[i];
+        mx.Add(vec.Map(y => func(x, y)));
+      }
+      return mx;
+    }
+    public static List<List<TO>> Cartesian<TA, TB, TO>(this IReadOnlyList<TA> vecA, IReadOnlyList<TB> vecB, Func<TA, TB, TO> func) {
+      var hi = vecA.Count;
+      var mx = new List<List<TO>>(hi);
+      for (var i = 0; i < hi; i++) {
+        var x = vecA[i];
+        mx.Add(vecB.Map(y => func(x, y)));
+      }
+      return mx;
+    }
+    public static List<List<TO>> IndoCartesian<T, TO>(this IReadOnlyList<T> vec, Func<int, int, T, T, TO> func) {
+      var hi = vec.Count;
+      var mx = new List<List<TO>>(hi);
+      for (var i = 0; i < hi; i++) {
+        var x = vec[i];
+        mx.Add(vec.Map((j, y) => func(i, j, x, y)));
+      }
+      return mx;
+    }
+    public static List<List<TO>> Cartesian<TA, TB, TO>(this IReadOnlyList<TA> vecA, IReadOnlyList<TB> vecB, Func<int, int, TA, TB, TO> func) {
+      var hi = vecA.Count;
+      var mx = new List<List<TO>>(hi);
+      for (var i = 0; i < hi; i++) {
+        var x = vecA[i];
+        mx.Add(vecB.Map((j, y) => func(i, j, x, y)));
+      }
+      return mx;
+    }
+  }
+}
---
+++ Veho.Vector/Src/Vector/Algebra.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Veho.Vector {
+  public static class Algebra {
+    public static TO[,] IndoCartesian<T, TO>(this IReadOnlyList<T> vec, Func<T, T, TO> func) {
+      var hi = vec.Count;
+      var mx = new TO[hi, hi];
+      for (var i = 0; i < hi; i++) {
+        var x = vec[i];
+        for (var j = 0; j < hi; j++) mx[i, j] = func(x, vec[j]);
+      } // i + 1
+      return mx;
+    }
+    public static TO[,] Cartesian<TA, TB, TO>(this IReadOnlyList<TA> vecA, IReadOnlyList<TB> vecB, Func<TA, TB, TO> func) {
+      int h = vecA.Count, w = vecB.Count;
+      var mx = new TO[h, w];
+      for (var i = 0; i < h; i++) {
+        var a = vecA[i];
+        for (var j = 0; j < w; j++) mx[i, j] = func(a, vecB[j]);
+      }
+      return mx;
+    }
+    public static TO[,] IndoCartesian<T, TO>(this IReadOnlyList<T> vec, Func<int, int, T, T, TO> func) {
+      var hi = vec.Count;
+      var mx = new TO[hi, hi];
+      for (var i = 0; i < hi; i++) {
+        var x = vec[i];
+        for (var j = 0; j < hi; j++) mx[i, j] = func(i, j, x, vec[j]);
+      } // i + 1
+      return mx;
+    }
+    public static TO[,] Cartesian<TA, TB, TO>(this IReadOnlyList<TA> vecA, IReadOnlyList<TB> vecB, Func<int, int, TA, TB, TO> func) {
+      int h = vecA.Count, w = vecB.Count;
+      var mx = new TO[h, w];
+      for (var i = 0; i < h; i++) {
+        var a = vecA[i];
+        for (var j = 0; j < w; j++) mx[i, j] = func(i, j, a, vecB[j]);
+      }
+      return mx;
+    }
+    
+  }
+}
