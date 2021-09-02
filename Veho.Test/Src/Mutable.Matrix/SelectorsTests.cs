8f7742bab6462235bd71add64203004eb0cffa09
hoyeungw <hoyeungw@outlook.com>
2021-09-02 12:16:21 +0800

    add SelectorsTest

---
+++ Veho.Test/Src/Mutable.Matrix/SelectorsTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Spare;
+using Veho.Mutable.Columns;
+using Veho.Mutable.Matrix;
+using MU = Veho.Mutable;
+
+namespace Veho.Test.Mutable.Matrix {
+  [TestFixture]
+  public class SelectorsTests {
+    [Test]
+    public void SelectByColumnIndicesTest() {
+      var rows = MU::Mat.Init(5, 5, (i, j) => i * j);
+      rows.ToMatrix().Deco().Says("rows");
+      rows.SelectByColumnIndices(Seq.From(1, 2, 4)).ToMatrix().Deco().Says("selected");
+    }
+  }
+}
