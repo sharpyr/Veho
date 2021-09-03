70f211ad6f61ebea653c5e95cc568d4115ef128c
hoyeungw <hoyeungw@outlook.com>
2021-09-03 12:19:35 +0800

    add SkipRange class

---
+++ Veho.Sequence/Src/SkipRange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Veho {
+  public class SkipRange {
+    public HashSet<int> Used { get; private set; }
+    public int Lower { get; set; }
+    public int Upper { get; set; }
+    public static SkipRange Build(int upper) => new SkipRange {
+      Lower = 0,
+      Upper = upper,
+      Used = new HashSet<int>()
+    };
+    public IEnumerable<int> IntoIter() {
+      for (var i = Lower; i < Upper; i++) {
+        if (Used.Contains(i)) continue;
+        yield return i;
+      }
+    }
+    public HashSet<int> AcquireIndices(IEnumerable<int> indices) {
+      this.Used.UnionWith(indices);
+      return this.Used;
+    }
+  }
+}
