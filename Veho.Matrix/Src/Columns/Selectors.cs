177d012214bf74f735545f96ecd293245863bf5a
hoyeungw <hoyeungw@outlook.com>
2021-09-03 12:18:01 +0800

    add SelectColumns for T[,]

---
+++ Veho.Matrix/Src/Columns/Selectors.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Veho.Matrix;
+
+namespace Veho.Columns {
+  public static class Selectors {
+    public static T[,] SelectColumns<T>(this T[,] rows, IReadOnlyList<int> columnIndices) {
+      int h = rows.Height(), w = columnIndices.Count;
+      var target = new T[h, w];
+      for (var i = 0; i < h; i++)
+        for (var j = 0; j < w; j++)
+          target[i, j] = rows[i, columnIndices[j]];
+      return target;
+    }
+  }
+}
