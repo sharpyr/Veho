efd2e8f74bf3658eb3792551d9a77ef28d722e0e
hoyeungw <hoyeungw@outlook.com>
2021-09-03 12:16:21 +0800

    add SelectBy for Mutable.Matrix

---
+++ Veho.Mutable.Matrix/Matrix/Selectors.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Veho.Sequence;
+
+namespace Veho.Mutable.Matrix {
+  public static class Selectors {
+    public static List<List<T>> SelectBy<T>(IReadOnlyList<IReadOnlyList<T>> rows, IReadOnlyList<int> sideIndices, IReadOnlyList<int> headIndices) {
+      return rows.SelectBy(sideIndices).Map(row => row.SelectBy(headIndices));
+    }
+  }
+}
