a11823c00d1f25e209da656842dfa16da286afa1
hoyeungw <hoyeungw@outlook.com>
2021-09-26 10:56:57 +0800

    add IntoColumnsIter for mutable matrix

--- Veho.Mutable.Matrix/Columns/Selectors.cs
+++ Veho.Mutable.Matrix/Columns/Selectors.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Veho.Mutable.Matrix;
 using Veho.Sequence;
 
 namespace Veho.Mutable.Columns {
@@ -8,6 +9,15 @@ namespace Veho.Mutable.Columns {
     }
     public static List<List<T>> SelectByColumnIndices<T>(this IReadOnlyList<IReadOnlyList<T>> rows, IReadOnlyList<int> columnIndices) {
       return rows.Map(row => row.SelectBy(columnIndices));
+    }
+    public static IEnumerable<List<T>> IntoColumnsIter<T>(this IReadOnlyList<IReadOnlyList<T>> matrix, IReadOnlyList<int> rowIndices) {
+      var width = matrix.Width();
+      var height = rowIndices.Count;
+      for (var j = 0; j < width; j++) {
+        var column = new List<T>(height);
+        for (var i = 0; i < height; i++) column.Add(matrix[rowIndices[i]][j]);
+        yield return column;
+      }
     }
   }
 }
