a4d23ccaa875eea20e113b41fa94996951a9fab5
hoyeungw <hoyeungw@outlook.com>
2021-09-26 10:57:08 +0800

    add IntoRowsIter for mutable matrix

---
+++ Veho.Mutable.Matrix/Rows/Selectors.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Veho.Mutable.Matrix;
+using Veho.Sequence;
+
+namespace Veho.Mutable.Rows {
+  public static class Selectors {
+    public static IEnumerable<List<T>> IntoRowsIter<T>(this IReadOnlyList<IReadOnlyList<T>> matrix, IReadOnlyList<int> columnIndices) {
+      var height = matrix.Height();
+      for (var i = 0; i < height; i++) {
+        yield return matrix[i].SelectBy(columnIndices);
+      }
+    }
+  }
+}
