bc3de83df8cb17b4d45de5525cd692b809d7826c
hoyeungw <hoyeungw@outlook.com>
2021-09-28 20:25:17 +0800

    add Zippers for IEnumerable<T>

---
+++ Veho.Enumerable/Src/Enumerable/Zippers.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Veho.Enumerable {
+  public static class Zippers {
+    public static IEnumerable<T> Zip<TA, TB, T>(this IEnumerable<TA> a, IEnumerable<TB> b, Func<TA, TB, T> fn) {
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext()) {
+          yield return fn(aI.Current, bI.Current);
+        }
+    }
+    public static IEnumerable<T> Zip<TA, TB, T>(this IEnumerable<TA> a, IEnumerable<TB> b, Func<int, TA, TB, T> fn) {
+      var i = 0;
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext()) {
+          yield return fn(i++, aI.Current, bI.Current);
+        }
+    }
+    public static void IterZip<TA, TB>(this IEnumerable<TA> a, IEnumerable<TB> b, Action<TA, TB> action) {
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext()) {
+          action(aI.Current, bI.Current);
+        }
+    }
+    public static void IterZip<TA, TB>(this IEnumerable<TA> a, IEnumerable<TB> b, Action<int, TA, TB> action) {
+      var i = 0;
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext()) {
+          action(i++, aI.Current, bI.Current);
+        }
+    }
+   
+    public static IEnumerable<T> Zipper<TA, TB, T>(this Func<TA, TB, T> fn, IEnumerable<TA> a, IEnumerable<TB> b) {
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext()) {
+          yield return fn(aI.Current, bI.Current);
+        }
+    }
+    public static IEnumerable<T> Zipper<TA, TB, TC, T>(this Func<TA, TB, TC, T> fn, IEnumerable<TA> a, IEnumerable<TB> b, IEnumerable<TC> c) {
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+      using (var cI = c.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext() && cI.MoveNext()) {
+          yield return fn(aI.Current, bI.Current, cI.Current);
+        }
+    }
+    public static IEnumerable<T> Zipper<TA, TB, TC, TD, T>(this Func<TA, TB, TC, TD, T> fn, IEnumerable<TA> a, IEnumerable<TB> b, IEnumerable<TC> c, IEnumerable<TD> d) {
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+      using (var cI = c.GetEnumerator())
+      using (var dI = d.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext() && cI.MoveNext() && dI.MoveNext()) {
+          yield return fn(aI.Current, bI.Current, cI.Current, dI.Current);
+        }
+    }
+    public static IEnumerable<T> Zipper<TA, TB, T>(this Func<int,TA, TB, T> fn, IEnumerable<TA> a, IEnumerable<TB> b) {
+      var i = 0;
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext()) {
+          yield return fn(i++,aI.Current, bI.Current);
+        }
+    }
+    public static IEnumerable<T> Zipper<TA, TB, TC, T>(this Func<int,TA, TB, TC, T> fn, IEnumerable<TA> a, IEnumerable<TB> b, IEnumerable<TC> c) {
+      var i = 0;
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+      using (var cI = c.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext() && cI.MoveNext()) {
+          yield return fn(i++,aI.Current, bI.Current, cI.Current);
+        }
+    }
+    public static IEnumerable<T> Zipper<TA, TB, TC, TD, T>(this Func<int,TA, TB, TC, TD, T> fn, IEnumerable<TA> a, IEnumerable<TB> b, IEnumerable<TC> c, IEnumerable<TD> d) {
+      var i = 0;
+      using (var aI = a.GetEnumerator())
+      using (var bI = b.GetEnumerator())
+      using (var cI = c.GetEnumerator())
+      using (var dI = d.GetEnumerator())
+        while (aI.MoveNext() && bI.MoveNext() && cI.MoveNext() && dI.MoveNext()) {
+          yield return fn(i++,aI.Current, bI.Current, cI.Current, dI.Current);
+        }
+    }
+  }
+}
