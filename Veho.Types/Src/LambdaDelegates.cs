0101b236567328c04674b47b700c263fef39fc01
hoyeungw <hoyeungw@outlook.com>
2021-09-28 20:28:33 +0800

    add LambdaDelegates

---
+++ Veho.Types/Src/LambdaDelegates.cs
@@ -0,0 +1,9 @@
+﻿namespace Veho.Types {
+  public delegate void ActionByRef<T>(ref T x);
+
+  public delegate void ActionByRef<T1, T2>(ref T1 x, ref T2 y);
+
+  public delegate void ActionByRef<T1, T2, T3>(ref T1 x, ref T2 y, ref T3 z);
+
+  public delegate void IndexedActionByRef<T>(ref T x, int idx);
+}
