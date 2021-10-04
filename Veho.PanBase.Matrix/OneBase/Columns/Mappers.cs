7a271755d7e663b28bdc5d804d744635f39a0371
hoyeungw <hoyeungw@outlook.com>
2021-10-05 06:09:35 +0800

    add ColumnsIter to 1-based T[,]

---
+++ Veho.PanBase.Matrix/OneBase/Columns/Mappers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Veho.Matrix;
+
+namespace Veho.OneBase.Columns {
+  public static class Mappers {
+    public static IEnumerable<T[]> ColumnsIter<T>(this T[,] matrix) {
+      var (height, width) = matrix.Size();
+      for (var j = 0; j < width; j++) {
+        yield return matrix.ZeroizeColumn(j, height);
+      }
+    }
+  }
+}
