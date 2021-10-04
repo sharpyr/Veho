474ca874bce4e07759aa74c2adf43a944db6fb7f
hoyeungw <hoyeungw@outlook.com>
2021-10-05 06:10:08 +0800

    add RowsIter for 1-based T[,]

---
+++ Veho.PanBase.Matrix/OneBase/Rows/Mappers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Veho.Matrix;
+
+namespace Veho.OneBase.Rows {
+  public static class Mappers {
+    public static IEnumerable<T[]> RowsIter<T>(this T[,] matrix) {
+      var (height, width) = matrix.Size();
+      for (var i = 0; i < height; i++) {
+        yield return matrix.ZeroizeRow(i, width);
+      }
+    }
+  }
+}
