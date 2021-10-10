392b1a8251ec63934930346405760676151af035
hoyeungw <hoyeungw@outlook.com>
2021-10-10 13:18:12 +0800

    add utils for Veho.Linear

---
+++ Veho.Linear/Src/Utils/LinearAlgebraHelper.cs
@@ -0,0 +1,7 @@
+﻿using Veho.Matrix;
+
+namespace Veho.Utils {
+  public static class LinearAlgebraHelper {
+    public static bool ColToRow<T>(this T[,] aX, T[,] bX) => aX.Width() == 1 && bX.Height() == 1;
+  }
+}
---
+++ Veho.Linear/Src/Utils/StringExt.cs
@@ -0,0 +1,7 @@
+﻿using Veho.Formula;
+
+namespace Veho.Utils {
+  public static class StringExt {
+    public static T Concatenate<T>(T a, T b) => Algebra.Convert<string, T>(a.ToString() + b.ToString());
+  }
+}
