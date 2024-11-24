04456a87505a31a98f2c786585933f05591023c9
hoyeungw <hoyeungw@outlook.com>
2024-11-24 20:42:23 +0800

    add Leaps and Bounds in Profile

--- Veho.Matrix/Src/Matrix/Profile.cs
+++ Veho.Matrix/Src/Matrix/Profile.cs
@@ -2,10 +2,12 @@
   public static class Profile {
     public static bool Any<T>(this T[,] matrix) => matrix.Size().Any();
     public static (int height, int width) Size<T>(this T[,] matrix) => (matrix.GetLength(0), matrix.GetLength(1));
+    public static (int xlo, int ht, int ylo, int wd) Leaps<T>(this T[,] matrix) => (matrix.GetLowerBound(0), matrix.GetLength(0), matrix.GetLowerBound(1), matrix.GetLength(1));
+    public static (int xlo, int xhi, int ylo, int yhi) Bounds<T>(this T[,] matrix) => (matrix.GetLowerBound(0), matrix.GetUpperBound(0), matrix.GetLowerBound(1), matrix.GetUpperBound(1));
     public static (int lo, int hi) XBound<T>(this T[,] matrix) => (matrix.GetLowerBound(0), matrix.GetUpperBound(0));
     public static (int lo, int hi) YBound<T>(this T[,] matrix) => (matrix.GetLowerBound(1), matrix.GetUpperBound(1));
-    public static (int lo, int dif) XLeap<T>(this T[,] matrix) => (matrix.GetLowerBound(0), matrix.GetLength(0));
-    public static (int lo, int dif) YLeap<T>(this T[,] matrix) => (matrix.GetLowerBound(1), matrix.GetLength(1));
+    public static (int lo, int ht) XLeap<T>(this T[,] matrix) => (matrix.GetLowerBound(0), matrix.GetLength(0));
+    public static (int lo, int wd) YLeap<T>(this T[,] matrix) => (matrix.GetLowerBound(1), matrix.GetLength(1));
     public static int XLo<T>(this T[,] matrix) => matrix.GetLowerBound(0);
     public static int YLo<T>(this T[,] matrix) => matrix.GetLowerBound(1);
     public static int XHi<T>(this T[,] matrix) => matrix.GetUpperBound(0);
